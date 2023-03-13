fae98ce317a30779cf9818f345cb17a5ba4e4d6b
PaJiOvO <1404492917@qq.com>
2023-03-13 11:55:10 +0000

    update src/main/java/GitTest.java.
    Signed-off-by: PaJiOvO <1404492917@qq.com>

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix Test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
     }
 }
