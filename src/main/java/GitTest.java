7e3bf41af2b53fb12a0d35881465e94fa113d0f1
PajiOvO <127723250+PajiOvO@users.noreply.github.com>
2023-03-13 19:28:23 +0800

    Update GitTest.java
    pull commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("master Test");
         System.out.println("hot-fix Test");
         System.out.println("push test");
+        System.out.println("push test");
     }
 }
